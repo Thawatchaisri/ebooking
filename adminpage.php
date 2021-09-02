dcf83fbede6afd70a2df00a12387126cd5d73ae1
Thawatchaisri <stanutty06@gmail.com>
2021-09-02 17:11:53 +0700

    add new admin

--- adminpage.php
+++ adminpage.php
@@ -1,17 +1,18 @@
 <head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+
 </head>
 
 <link link rel="stylesheet" href="admin.css">
 
-<ul  >
+<ol>
   <li><a href="#">Home</a></li>
   <li><a href="#">งานแจ้งซ่อม</a></li>
   <li><a href="#">ติดตามการแจ้งซ่อม</a></li>
   <li><a href="#">จัดการสมาชิก</a></li>
   <li><a href="#">จัดการแผนก</a></li>
   <li><a href="#">Report</a></li>
-  <li ><a href="#">logout</a></li>
-</ul>
+  <li><a class="textred" href="index.php">logout</a></li>
+</ol>
+<body>
+<div>sdfsdf</div>
+</body>
