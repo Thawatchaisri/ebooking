4cec2efee47b469076bfa57c08fb40629d0455dd
Thawatchaisri <stanutty06@gmail.com>
2021-09-02 15:19:29 +0700

    inital commit

---
+++ adminpage.php
@@ -0,0 +1,8 @@
+<link link rel="stylesheet" href="admin.css">
+
+<ul>
+  <li><a href="default.html">Home</a></li>
+  <li><a href="Jobs">News</a></li>
+  <li><a href="Manage Member ">Contact</a></li>
+  <li><a href="Report">About</a></li>
+</ul>
